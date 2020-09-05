5efcac87cbe8e8999c758bddc802c5a68304e0b0
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2020-09-04 21:10:49 -0400

    First (possibly final) iteration.
    This pretty much does the job. A first major improvement would
    be to output three columns, not one (such that the column to the
    right is a continuation of the column to the left, as in a newspaper.)
    
    Also, column height would be enforced to be something like 48 (which
    seems to work in Libreoffice.) So you'd end up with chunks that are
    48 lines high, and that are situated next to each other, but the fourth
    one belongs to a new set.
    
    You'd likely need some data structure to fit the output of the
    iterator, to enforce this topology of the output.

--- report.lua
+++ report.lua
@@ -1,3 +1,4 @@
+#!/usr/bin/env lua
 --[[
 	Tool meant to simplify each line into the form
 
@@ -9,3 +10,34 @@ Ideally, each line should be multi-column, to save space.
 
 Also, I should only need to go back as far as the last three months.
 ]]
+
+require "strict"
+
+local filename = arg[1] -- should be a csv file
+assert(filename)
+
+local extension = filename:sub(-4)
+assert(extension == ".csv")
+
+local file = io.open(filename)
+
+local function entries (file, limit) -- '06/30/2020', first non-applicable date
+	local next_line = file:lines()
+
+	next_line() -- throw out the first line
+
+	return function ()
+		local line = next_line()
+
+		local date, midday, evening = line:match("^(.-),(.-),.-,(.-),")
+
+		if date == limit then return nil end
+
+		return date, midday, evening
+	end
+end
+
+for d, m, e in entries(file, "05/31/2020") do
+	local chunk = string.format("%s %s %s", d, m, e)
+	print(chunk)
+end
