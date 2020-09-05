3e400ed5a9c48b360ad7d3334e2745b1020b9ad9
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2020-09-04 20:31:21 -0400

    Initial commit.
    Just a '.gitignore' meant to ignore the data files, and the
    initial stub of 'report.lua', meant to generate the final
    "bookie" report. :)

---
+++ report.lua
@@ -0,0 +1,11 @@
+--[[
+	Tool meant to simplify each line into the form
+
+date midday evening
+date midday evening
+...
+
+Ideally, each line should be multi-column, to save space.
+
+Also, I should only need to go back as far as the last three months.
+]]
